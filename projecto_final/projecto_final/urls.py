79ad664c21187953a7f04a01a7c04b6d4353f273
Bill Gutierrez <darckneck@hotmail.com>
2023-12-03 23:59:31 -0500

    Agenda y tareas version 1

---
+++ projecto_final/projecto_final/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('agenda/', include('agenda.urls')),
+    path('tareas/', include('tareas.urls')),
+]
